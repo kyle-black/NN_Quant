1dfb45563bab149c84823ac3d58e4a1ee6e54691
kyle-black <kyle-black@FalconXtreme.localdomain>
2025-09-23 12:46:28 -0500

    updated features pipelines

--- src/train_nn.py
+++ src/train_nn.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     horizon = 20
     thr =0.005
     splits = 5
-    test_size = 0.20
+    test_size = 0.30
     epochs =10
     batch_size= 256
     out = "models/eurusd_nn"
